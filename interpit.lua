f04a59e08514c5a3f5ae8eb75c2a5a953eb0d1e8
Corey Gray <csgray@alaska.edu>
2018-04-15 13:53:48 -0800

    Pass simple programs and modified initial state tests.

--- interpit.lua
+++ interpit.lua
@@ -180,19 +180,22 @@ function interpit.interp(ast, state, incall, outcall)
     -- versions of state, incall, and outcall may be used. The
     -- function-wide version of state may be modified as appropriate.
 
-
     function interp_stmt_list(ast)
         for i = 2, #ast do
             interp_stmt(ast[i])
         end
     end
 
-
     function interp_stmt(ast)
         local name, body, str
 
         if ast[1] == INPUT_STMT then
-            print("Input stmt; DUNNO WHAT TO DO!!!")
+            if ast[2][1] == SIMPLE_VAR then
+              name = ast[2][2]
+              body = incall()
+              state.v[name] = numToInt(strToNum(body))
+            end
+        
         elseif ast[1] == PRINT_STMT then
             for i = 2, #ast do
                 if ast[i][1] == CR_OUT then
@@ -201,13 +204,16 @@ function interpit.interp(ast, state, incall, outcall)
                     str = ast[i][2]
                     outcall(str:sub(2,str:len()-1))  -- Remove quotes
                 else
-                    print("Print stmt with expression; DUNNO WHAT TO DO!!!")
+                    body = evalExpr(ast[2])
+                    outcall(numToStr(body))
                 end
             end
+        
         elseif ast[1] == FUNC_STMT then
             name = ast[2]
             body = ast[3]
             state.f[name] = body
+        
         elseif ast[1] == CALL_FUNC then
             name = ast[2]
             body = state.f[name]
@@ -215,25 +221,88 @@ function interpit.interp(ast, state, incall, outcall)
                 body = { STMT_LIST }  -- Default AST
             end
             interp_stmt_list(body)
+        
         elseif ast[1] == IF_STMT then
-            print("If stmt; DUNNO WHAT TO DO!!!")
+            local done = false
+            local elseIndex = 0
+            
+            for i = 2, #ast - 1, 2 do
+              if evalExpr(ast[i]) ~= 0 and not done then
+                interp_stmt_list(ast[i + 1])
+                done = true
+              end
+              elseIndex = i
+            end
+              
+            if ast[elseIndex + 2] and not done then
+              interp_stmt_list(ast[elseIndex + 2])
+            end
+            
         elseif ast[1] == WHILE_STMT then
-            print("While stmt; DUNNO WHAT TO DO!!!")
+            while evalExpr(ast[2]) ~= 0 do
+              interp_stmt_list(ast[3])
+            end
+            
         else
             assert(ast[1] == ASSN_STMT)
-            print("Assignment stmt; DUNNO WHAT TO DO!!!")
+            processLValue(ast)
         end
     end
 
+  function processLValue(ast)
+    local name, body
+    
+    if ast[2][1] == SIMPLE_VAR then
+      name = ast[2][2]
+      body = evalExpr(ast[3])
+      state.v[name] = numToInt(body)
+      
+    elseif ast[2][1] == ARRAY_VAR then
+      name = ast[2][2]
+      if state.a[name] == nil then
+        state.a[name] = {}
+      end
+      body = evalExpr(ast[3])
+      state.a[name][evalExpr(ast[2][3])] = body
+    end
+  end
 
-    -- Body of function interp
-    interp_stmt_list(ast)
-    return state
+  function evalExpr(ast)
+    local value
+    
+    if ast[1] == NUMLIT_VAL then
+      value = strToNum (ast[2])
+      
+    elseif ast[1] == SIMPLE_VAR then
+      value = state.v[ast[2]]
+      
+    elseif ast[1] == ARRAY_VAR then
+      if state.a[ast[2]] ~= nil then
+        value = state.a[ast[2]][evalExpr(ast[3])]
+      else
+        value = 0
+      end
+      
+    elseif ast[1] == BOOLLIT_VAL then
+      value = boolToInt(strToBool(ast[2]))
+      
+    elseif ast[1] == CALL_FUNC then
+      interp_stmt(ast)
+      value = state.v["return"]
+    end
+    
+    if value == nil then
+      return 0
+    else
+      return value
+    end
+  end
+
+  
+  -- Body of function interp
+  interp_stmt_list(ast)
+  return state
 end
 
-
 -- ***** Module Export *****
-
-
 return interpit
-
